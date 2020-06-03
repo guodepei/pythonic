85580eeeba89d805100a03bbb7b4cb45f650dbda
Garfield <guodepei@gmail.com>
2020-06-03 14:08:40 +0800

    curtail to one-page

--- pythonic.py
+++ pythonic.py
@@ -1,41 +1,24 @@
 #!/bin/python3
-# pythonic.py - a demo of python3 pythonic programming
+# a one-page demo of pythonic programming
 
-'''A one file demo of python3 pythonic programming
+a, b = 1, 2
+a, b = b, a # 1、变量交换
 
-For usage, try this:
-$ python pythonic.py
-'''
-
-# 1、变量交换
-a = 1
-b = 2
-a, b = b, a
-print(a, b)
-
-# 2、循环遍历区间元素
-for i in range(6):
+for i in range(6): # 2、循环遍历区间元素
     print(i)
 
-# 3、带有索引位置的集合遍历
 colors = ['red', 'green', 'blue', 'yellow']
-for i, color in enumerate(colors):
+for i, color in enumerate(colors): # 3、带有索引位置的集合遍历
     print(i, '--->', color)
 
-# 4、字符串连接
-names = ['raymond', 'rachel', 'matthew', 'roger',
-         'betty', 'melissa', 'judith', 'charlie']
-print(', '.join(names))
+names = ['raymond', 'rachel', 'matthew', 'roger']
+print(', '.join(names)) # 4、字符串连接
 
-# 5、打开 / 关闭文件
-with open('README.md') as f:
+with open('README.md') as f: # 5、打开 / 关闭文件
     data = f.read()
-    print(data)
 
-# 6、列表推导式
-print([i * 2 for i in range(10)])
+print([i * 2 for i in range(10)]) # 6、列表推导式
 
-# 7、善用装饰器
 import urllib.request as urllib # py3
 
 def cache(func):
@@ -49,26 +32,18 @@ def cache(func):
             return page
     return wrapper
 
-@cache
+@cache # 7、善用装饰器
 def web_lookup(url):
     return urllib.urlopen(url).read()
 
-print(web_lookup("https://bing.com"))
-print(web_lookup("https://bing.com"))
-
-# 8、合理使用列表
-from collections import deque
-names = deque(['raymond', 'rachel', 'matthew', 'roger',
-               'betty', 'melissa', 'judith', 'charlie'])
+from collections import deque # 8、合理使用列表
+names = deque(['raymond', 'rachel', 'matthew', 'roger'])
 names.popleft()
 names.appendleft('mark')
-print(names)
 
-# 9、序列解包
 p = 'vttalk', 'female', 30, 'python@qq.com'
-name, gender, age, email = p
+name, gender, age, email = p # 9、序列解包
 
-# 10、遍历字典的
 d = {"red":1, "blue":3, "green": 5}
-for k, v in d.items():
+for k, v in d.items(): # 10、遍历字典的
     print(k, '--->', v)
